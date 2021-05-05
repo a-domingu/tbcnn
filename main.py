1b8715d7188247ff6ed94ba12511fa34b5f45071
adomingu <alfonso.dominguez-de-la-iglesia-external@capgemini.com>
2021-05-05 11:24:19 +0200

    main.py now receives arguments

--- main.py
+++ main.py
@@ -25,17 +25,8 @@ from validation_neural_network import Validation_neural_network
 
 #####################################
 # SCRIPT
-def main():
-    ### Inicializar todos los parametros
-    # First neural network parameters
-    vector_size = 20
-    learning_rate = 0.1
-    momentum = 0.01
-    # Second neural network parameters
-    learning_rate2 = 0.1
-    feature_size = 4
-    epoch = 10
-    pooling = 'one-way pooling'
+def main(vector_size, learning_rate, momentum, learning_rate2, feature_size, epoch, pooling):
+
 
     ### Creation of the training set and validation set
     path = os.path.join('sets', 'generators')
@@ -149,4 +140,16 @@ def first_neural_network(training_dict, vector_size = 20, learning_rate = 0.1, m
 
 
 if __name__ == '__main__':
-    main()
+    #first neural network parameters
+    vector_size = 20
+    learning_rate = 0.1
+    momentum = 0.01
+    # Second neural network parameters
+    learning_rate2 = 0.1
+    feature_size = 4
+    epoch = 10
+    pooling = 'one-way pooling'
+
+    main(vector_size, learning_rate, momentum, learning_rate2, feature_size, epoch, pooling)
+
+
---
+++ param_tester.py
@@ -0,0 +1,24 @@
+from main import main
+
+
+# TODO asignar los valores que queramos para cada caso
+
+vector_size_ls = [20]
+
+learning_rate_ls = [0.1]
+
+momentum_ls = [0.01]
+
+# Second neural network parameters
+feature_size_ls = [4]
+epoch = 10
+pooling = 'one-way pooling'
+
+for vector_size in vector_size_ls:
+    for learning_rate in learning_rate_ls:
+        for momentum in momentum_ls:
+            for learning_rate2 in [0.1]:
+                for feature_size in feature_size_ls:
+                    # TODO llamar aqui al main
+                    pass
+
